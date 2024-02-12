edf8ca3d0e5ffbed3424cabfb2ed590fdfacb02f
Sercrac07 <sercassan@gmail.com>
2024-02-12 17:14:37 +0100

    Added leveling system

--- niveles/script.js
+++ niveles/script.js
@@ -1,1 +1,19 @@
+import { $, createLevelStorage } from '../js/index.js';
 
+const $map = $('map');
+
+let nivelesStorage = localStorage.getItem('niveles');
+
+if (!nivelesStorage) {
+  createLevelStorage();
+
+  nivelesStorage = localStorage.getItem('niveles');
+}
+
+const nivelesUser = JSON.parse(nivelesStorage);
+
+nivelesUser.forEach(nivel => {
+  $map.innerHTML += `<button onclick="location.href = 'nivel${nivel.id}'" ${
+    nivel.enabled ? '' : ' disabled'
+  }>${nivel.name}</button>`;
+});
