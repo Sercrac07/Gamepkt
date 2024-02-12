dc7cf5e37ab8e1197a93f4e2cf98299b8f672db5
Sercrac07 <sercassan@gmail.com>
2024-02-12 14:42:36 +0100

    Added document function

---
+++ js/document.js
@@ -0,0 +1,7 @@
+/**
+ * Recupera un elemento del HTML
+ * @param {string} str ID del elemento a recuperar
+ */
+export function $(str) {
+  return document.getElementById(str);
+}
--- js/index.js
+++ js/index.js
@@ -1,1 +1,3 @@
 export { toFirstUpperCase } from './string.js';
+export { $ } from './document.js';
+export { createLevelStorage, handleLevel, handleMissions } from './levels.js';
